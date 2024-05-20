cdf361dedff389770ebb56bcb9c0afb7035b1816
David Drobner <david@drobner.ca>
2024-05-20 00:13:05 -0400

    Copy track class into it's own file

---
+++ engineutils/track.py
@@ -0,0 +1,28 @@
+# a file to store the track class for future reuse
+
+from pathlib import Path
+from os.path import abspath
+
+class Track:
+    def __init__(self, track_id: int, track_path: Path):
+        self._id = track_id
+        self._path = Path(abspath(track_path))
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def path(self):
+        return self._path
+    
+    @id.setter
+    def id(self, val: int):
+        self._id = val 
+    
+    @path.setter
+    def path(self, val: Path):
+        self._path = abspath(val) 
+
+    def __str__(self):
+        return f"ID: {self.id} Path: {self.path}"
